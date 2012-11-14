7ff8b601eed63e6a7bacb713489ae8fb52103f0d
Nathan Rajlich <nathan@tootallnate.net>
2012-11-14 10:50:49 -0800

    find-ogg: use require.resolve() to find the "ogg" module

--- find-ogg.js
+++ find-ogg.js
@@ -10,23 +10,27 @@
 var fs = require('fs');
 var path = require('path');
 var module_root_dir = process.argv[2] || process.cwd();
-var dirs = [
-  // an "ogg" dir in the node_modules dir (direct dependency)
-  path.join(module_root_dir, 'node_modules', 'ogg'),
-  // an "ogg" dir in the parent dir (node_modules, shared dependency)
-  path.join(module_root_dir, '..', 'ogg'),
-  // a "node-ogg" git clone in the parent dir (developemnt, etc.)
-  path.join(module_root_dir, '..', 'node-ogg')
-];
-dirs.forEach(function (dir) {
-  try {
-    var stat = fs.statSync(dir);
-    if (stat.isDirectory()) {
-      console.log(dir);
-      process.exit(0);
-    }
-  } catch (e) {
-    if (e.code != 'ENOENT') throw err;
+
+try {
+  // first try to depend on node's module lookup algorithm
+  console.log(path.dirname(require.resolve('ogg')));
+  process.exit(0);
+} catch (e) {
+  // ignore
+}
+
+// next check if a "node-ogg" dir exists in the parent dir.
+// this would imply a git clone, i.e. local development of the module.
+var devPath = path.join(module_root_dir, '..', 'node-ogg');
+try {
+  var stat = fs.statSync(devPath);
+  if (stat.isDirectory()) {
+    console.log(devPath);
+    process.exit(0);
   }
-});
+} catch (e) {
+  if (e.code != 'ENOENT') throw err;
+}
+
+// fail :(
 throw new Error('could not find "node-ogg" installation!');
