25fb5308c8b3399228eebe9d4fa51847f9f62461
Nathan Rajlich <nathan@tootallnate.net>
2012-11-14 20:43:19 -0800

    find-ogg: greatly simplify the find-ogg script, now that "ogg" is a dep

--- find-ogg.js
+++ find-ogg.js
@@ -7,30 +7,7 @@
  * work on Windows.
  */
 
-var fs = require('fs');
 var path = require('path');
-var module_root_dir = process.argv[2] || process.cwd();
 
-try {
-  // first try to depend on node's module lookup algorithm
-  console.log(path.dirname(require.resolve('ogg')));
-  process.exit(0);
-} catch (e) {
-  // ignore
-}
-
-// next check if a "node-ogg" dir exists in the parent dir.
-// this would imply a git clone, i.e. local development of the module.
-var devPath = path.join(module_root_dir, '..', 'node-ogg');
-try {
-  var stat = fs.statSync(devPath);
-  if (stat.isDirectory()) {
-    console.log(devPath);
-    process.exit(0);
-  }
-} catch (e) {
-  if (e.code != 'ENOENT') throw err;
-}
-
-// fail :(
-throw new Error('could not find "node-ogg" installation!');
+// we depend on node's module lookup algorithm
+console.log(path.dirname(require.resolve('ogg')));
