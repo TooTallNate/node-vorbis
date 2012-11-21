27032b645629cf73cabb277178d7872c99e392ad
Nathan Rajlich <nathan@tootallnate.net>
2012-11-20 21:42:24 -0800

    preload node-ogg so the libogg symbols are loaded
    Required for Windows...

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 
+/**
+ * We have to load node-ogg in order for the libogg symbols
+ * to be visible on Windows.
+ */
+
+require('ogg');
+
 exports.Decoder = require('./lib/decoder');
+
 //exports.Encoder = require('./lib/encoder');
