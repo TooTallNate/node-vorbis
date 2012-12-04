fc6d26ea0c0607133545638d4d43e0a9aa0dc0fc
Nathan Rajlich <nathan@tootallnate.net>
2012-12-03 18:46:56 -0800

    test: add first Decoder test

---
+++ test/decoder.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var fs = require('fs');
+var ogg = require('ogg');
+var path = require('path');
+var vorbis = require('../');
+var assert = require('assert');
+var fixtures = path.resolve(__dirname, 'fixtures');
+
+describe('Decoder', function () {
+
+  describe('Rondo_Alla_Turka.ogg', function () {
+    var fixture = path.resolve(fixtures, 'Rondo_Alla_Turka.ogg');
+
+    it('should emit an "end" event', function (done) {
+      this.test.slow(5000);
+      this.test.timeout(10000);
+
+      var od = new ogg.Decoder();
+      od.on('stream', function (stream) {
+        var vd = new vorbis.Decoder(stream);
+        vd.on('end', done);
+
+        // flow...
+        vd.resume();
+      });
+      fs.createReadStream(fixture).pipe(od);
+    });
+
+  });
+
+});
